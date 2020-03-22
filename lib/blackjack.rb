a0c235d77a593914594538535df5712fefeb1edf
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-22 17:46:02 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,10 +36,21 @@ def initial_round
   card_sum
 end
 
-def hit?(card_total)
+def hit?(current_total)
   # code hit? here
   prompt_user
+  user_input = get_user_input
+  if user_input == "h"
+    current_total = current_total + deal_card
+    display_card_total(current_total)
+    current_total
+  else user_input == "s"
+    current_total
+  else
+    invalid_command
+    prompt_user
   get_user_input
+  end
 end
 
 def invalid_command
